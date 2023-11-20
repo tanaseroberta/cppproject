c558b6cf7851eea34140de9fd549c1de15abcb7e
Tanase Roberta <tanaseroberta22@stud.ase.ro>
2023-11-20 17:11:24 +0200

    test3

--- Project/Project/Source.cpp
+++ Project/Project/Source.cpp
@@ -1,6 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include<string>
 using namespace std;
+class Ticket {
+
+	const string ticketId;
+	int rowNumber;
+	int seatNumber;
+	bool isReserved;
+};
+enum ZoneName {
+	NORMAL = 1, PREMIUM = 2, VIP = 3
+};
+class Zone {
+
+	ZoneName name;
+	int maxRowNumber;
+	int maxSeatsPerRow;
+	int numberOfTickets;
+	Ticket* ticket;
+};
 class Location {
-	char* name; //dynamically defined field managed by a pointer
+
+	string name;
+	int maximumCapacity;
+	int numberOfZones;
+	Zone* zones;
+};
+class Event {
+
+	string eventName;
+	Location location;
 };
